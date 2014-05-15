b60d0c815374bc0bb6e4154f33123b94cab20c58
lolportal <fpalta90@gmail.com>
2014-05-15 18:12:03 +0400

    started making zoomInPlace function
    Signed-off-by: lolportal <fpalta90@gmail.com>

--- main.cpp
+++ main.cpp
@@ -58,35 +58,66 @@ void onZoomOutTrackbar(int pos, void* ){
 	cv::waitKey(1);
 }
 
+bool resizeInPlace( cv::Mat inputArray, cv::Mat outputArray, cv::Point zoomCenter, double scale){
+	if (inputArray.size().width != outputArray.size().width || inputArray.size().height != outputArray.size().height) return false;
+	if scale
+	cv::Size orig_size = inputArray.size();
+	int x_delta = zoomCenter.x - orig_size.width/2;
+	int y_delta = zoomCenter.y - orig_size.height/2;
+
+	cv::Size ROI_size(orig_size.width - abs(x_delta), orig_size.height - abs(y_delta));
+
+	int ROI_origin_x = (x_delta < 0) ? 0 : x_delta;
+	int ROI_origin_y = (y_delta < 0) ? 0 : y_delta;
+
+	cv::Point ROI_origin(ROI_origin_x,ROI_origin_y);
+	cv::Rect ROI_rect(ROI_origin,ROI_size);
+	cv::Mat zoom_center_ROI(inputArray,ROI_rect);
+
+	cv::Mat temp(orig_size,inputArray.type(),CV_RGB(0,0,0));
+	cv::Point temp_ROI_origin(abs(x_delta - ROI_origin_x), abs(y_delta - ROI_origin_y));
+	cv::Rect temp_ROI_rect(temp_ROI_origin,ROI_size);
+	cv::Mat temp_ROI(temp,temp_ROI_rect);
+	zoom_center_ROI.copyTo(temp_ROI);
+
+	if (scale >= 1.0)
+	return true;
+}
+
+
 void onMouse(int event, int x, int y, int, void* userdata){
 	if (event == CV_EVENT_LBUTTONDOWN){
 		cv::Mat img = *(cv::Mat*) userdata;
-		cv::Size orig_size = img.size();
-		int x_delta = x - orig_size.width/2;
-		int y_delta = y - orig_size.height/2;
+// 		cv::Size orig_size = img.size();
+// 		int x_delta = x - orig_size.width/2;
+// 		int y_delta = y - orig_size.height/2;
+// 
+// 		cv::Size ROI_size(orig_size.width - abs(x_delta), orig_size.height - abs(y_delta));
+// 
+// 		int ROI_origin_x = (x_delta < 0) ? 0 : x_delta;
+// 		int ROI_origin_y = (y_delta < 0) ? 0 : y_delta;
+// 
+// 		cv::Point ROI_origin(ROI_origin_x,ROI_origin_y);
+// 		cv::Rect ROI_rect(ROI_origin,ROI_size);
+// 		cv::Mat zoom_center_ROI(img,ROI_rect);
+// 		
+// 		cv::Mat temp(orig_size,img.type(),CV_RGB(0,0,0));
+// 		cv::Point temp_ROI_origin(abs(x_delta - ROI_origin_x), abs(y_delta - ROI_origin_y));
+// 		cv::Rect temp_ROI_rect(temp_ROI_origin,ROI_size);
+// 		cv::Mat temp_ROI(temp,temp_ROI_rect);
+// 		zoom_center_ROI.copyTo(temp_ROI);
+// 	
+// 		cv::imshow(zoomINwindow,temp);
+// 		cv::imshow(zoomOUTwindow,temp);
+// 
+// 		temp.copyTo(working_frame);
+// 		click_point = cv::Point(x,y);
+// 		cv::createTrackbar(zoomINtrackbar,zoomINwindow,&trackbar_pos,max_zoom,onZoomInTrackbar,&working_frame);
+// 		cv::createTrackbar(zoomOUTtrackbar,zoomOUTwindow,&trackbar_pos,max_zoom,onZoomOutTrackbar,&working_frame);
+// 		cv::waitKey(1);
 
-		cv::Size ROI_size(orig_size.width - abs(x_delta), orig_size.height - abs(y_delta));
-
-		int ROI_origin_x = (x_delta < 0) ? 0 : x_delta;
-		int ROI_origin_y = (y_delta < 0) ? 0 : y_delta;
-
-		cv::Point ROI_origin(ROI_origin_x,ROI_origin_y);
-		cv::Rect ROI_rect(ROI_origin,ROI_size);
-		cv::Mat zoom_center_ROI(img,ROI_rect);
-		
-		cv::Mat temp(orig_size,img.type(),CV_RGB(0,0,0));
-		cv::Point temp_ROI_origin(abs(x_delta - ROI_origin_x), abs(y_delta - ROI_origin_y));
-		cv::Rect temp_ROI_rect(temp_ROI_origin,ROI_size);
-		cv::Mat temp_ROI(temp,temp_ROI_rect);
-		zoom_center_ROI.copyTo(temp_ROI);
-	
-		cv::imshow(zoomINwindow,temp);
-		cv::imshow(zoomOUTwindow,temp);
-
-		temp.copyTo(working_frame);
-		click_point = cv::Point(x,y);
-		cv::createTrackbar(zoomINtrackbar,zoomINwindow,&trackbar_pos,max_zoom,onZoomInTrackbar,&working_frame);
-		cv::createTrackbar(zoomOUTtrackbar,zoomOUTwindow,&trackbar_pos,max_zoom,onZoomOutTrackbar,&working_frame);
+		resizeInPlace(img,img,cv::Point(x,y),0.5);
+		imshow("desu",img);
 		cv::waitKey(1);
 	}
 }
